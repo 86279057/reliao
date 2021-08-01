66315b26b2a874e2b1825958685a2a728bd8de50
saltedyu <86279057@qq.com>
2021-08-01 10:58:46 +0800

    Create location.js

---
+++ location.js
@@ -0,0 +1,19 @@
+
+//var body = $request.body;
+//body['longitude'] = '116.288160';
+//body['latitude'] = '39.177440';
+
+//body = JSON.stringify(body)
+//  $done({
+//    body
+//  })
+
+let body = JSON.parse($request.body);
+
+body.longitude ='116.3201' + Math.floor(Math.random() * 100);
+body.latitude ='39.9894' + Math.floor(Math.random() * 100);
+
+
+body = JSON.stringify(body);
+console.log(body);
+$done({body});
