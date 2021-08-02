95a716abf11a34abd395fabc6fd30a3c3a93caa5
saltedyu <86279057@qq.com>
2021-08-02 11:07:47 +0800

    Update location.js

--- location.js
+++ location.js
@@ -10,10 +10,23 @@
 
 let body = JSON.parse($request.body);
 
-body.longitude ='116.3201' + Math.floor(Math.random() * 100);
-body.latitude ='39.9894' + Math.floor(Math.random() * 100);
+body.longitude ='116.3207' + Math.floor(Math.random() * 100);
+body.latitude ='39.9911' + Math.floor(Math.random() * 100);
 
 
 body = JSON.stringify(body);
 console.log(body);
 $done({body});
+
+
+/*
+
+{
+  "os": "iOS",
+  "longitude": "116.320763",
+  "latitude": "39.991123",
+  "vpn": "true",
+  "sign": "f74c5391e912fcf5bed6b72e7a7ed41b",
+  "token": "f245d29f-1da7-4792-97d0-7f8a0047486b"
+}
+*/
